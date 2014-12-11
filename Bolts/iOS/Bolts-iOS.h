f48d785cdf0580841d41516afbc29e10ecc0c075
Justin Makaila <justinmakaila@gmail.com>
2014-12-11 17:38:09 -0500

    Added new ios header

---
+++ Bolts/iOS/Bolts-iOS.h
@@ -0,0 +1,29 @@
+//
+//  Bolts-iOS.h
+//  Bolts-iOS
+//
+//  Created by Justin Makaila on 12/11/14.
+//  Copyright (c) 2014 Parse Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bolts-iOS.
+FOUNDATION_EXPORT double BoltsVersionNumber;
+
+//! Project version string for Bolts-iOS.
+FOUNDATION_EXPORT const unsigned char BoltsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bolts/PublicHeader.h>
+
+#import <Bolts/BoltsVersion.h>
+#import <Bolts/BFExecutor.h>
+#import <Bolts/BFTask.h>
+#import <Bolts/BFTaskCompletionSource.h>
+#import <Bolts/BFAppLinkNavigation.h>
+#import <Bolts/BFAppLink.h>
+#import <Bolts/BFAppLinkTarget.h>
+#import <Bolts/BFURL.h>
+#import <Bolts/BFMeasurementEvent.h>
+#import <Bolts/BFAppLinkReturnToRefererController.h>
+#import <Bolts/BFAppLinkReturnToRefererView.h>
