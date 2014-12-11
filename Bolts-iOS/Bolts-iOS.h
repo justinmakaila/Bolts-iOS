b037679c18c392ca660d7c0e1f97112db33285b9
Justin Makaila <justinmakaila@gmail.com>
2014-12-11 16:03:04 -0500

    Added Bolts-iOS framework target

---
+++ Bolts-iOS/Bolts-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Bolts-iOS.h
+//  Bolts-iOS
+//
+//  Created by Justin Makaila on 12/11/14.
+//  Copyright (c) 2014 Parse Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bolts-iOS.
+FOUNDATION_EXPORT double Bolts_iOSVersionNumber;
+
+//! Project version string for Bolts-iOS.
+FOUNDATION_EXPORT const unsigned char Bolts_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bolts_iOS/PublicHeader.h>
+
+
