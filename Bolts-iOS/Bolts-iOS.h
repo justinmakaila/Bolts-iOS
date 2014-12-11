545ee1e2142c3269c6edef4b68512f0781dfcc8c
Justin Makaila <justinmakaila@gmail.com>
2014-12-11 16:29:01 -0500

    Updated Bolts-iOS header to include all headers for iOS

--- Bolts-iOS/Bolts-iOS.h
+++ Bolts-iOS/Bolts-iOS.h
@@ -16,4 +16,15 @@ FOUNDATION_EXPORT const unsigned char Bolts_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Bolts_iOS/PublicHeader.h>
 
-
+#import <Bolts/BFTask.h>
+#import <Bolts/BFTaskCompletionSource.h>
+#import <Bolts/BFExecutor.h>
+#import <Bolts/BFAppLink.h>
+#import <Bolts/BFAppLinkNavigation.h>
+#import <Bolts/BFAppLinkResolving.h>
+#import <Bolts/BFWebViewAppLinkResolver.h>
+#import <Bolts/BFAppLinkReturnToRefererController.h>
+#import <Bolts/BFAppLinkReturnToRefererView.h>
+#import <Bolts/BFAppLinkTarget.h>
+#import <Bolts/BFMeasurementEvent.h>
+#import <Bolts/BFURL.h>
